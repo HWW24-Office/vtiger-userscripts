c9c4c9b3c91704420a4a2253f021b3876f36e407
Hardwarewartung 24 GmbH <office@hardwarewartung.com>
2026-01-19 11:23:35 +0100

    Loader vtiger-product-number-tools-loader.user.js
    https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/vtiger-product-number-tools-loader.user.js

---
+++ vtiger-product-number-tools-loader.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         VTiger Product Number Tools (Loader)
+// @namespace    hw24.product.numbertools.loader
+// @version      1.0.0
+// @description  Loader for the central Product Number Tools userscript
+// @match        https://vtiger.hardwarewartung.com/index.php*
+// @grant        none
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/vtiger-product-number-tools.user.js
+// @updateURL    https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/vtiger-product-number-tools.user.js
+// @downloadURL  https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/vtiger-product-number-tools.user.js
+// ==/UserScript==
