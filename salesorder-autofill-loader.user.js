e1f54268f611177d23e29fc183dbfc6f2694a3f6
Hardwarewartung 24 GmbH <office@hardwarewartung.com>
2026-01-19 11:23:02 +0100

    Loader salesorder-autofill-loader.user.js
    https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/salesorder-autofill-loader.user.js

---
+++ salesorder-autofill-loader.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         VTiger SalesOrder Auto-Fill (Loader)
+// @namespace    hw24.salesorder.autofill.loader
+// @version      1.0.0
+// @description  Loader for the central SalesOrder Auto-Fill userscript
+// @match        https://vtiger.hardwarewartung.com/index.php*
+// @grant        none
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/salesorder-autofill.user.js
+// @updateURL    https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/salesorder-autofill.user.js
+// @downloadURL  https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/salesorder-autofill.user.js
+// ==/UserScript==
