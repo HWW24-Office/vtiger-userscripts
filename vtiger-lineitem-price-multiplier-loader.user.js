e88be822447632d244cad148033d766701f17b3f
Hardwarewartung 24 GmbH <office@hardwarewartung.com>
2026-01-19 11:24:04 +0100

    Loader vtiger-lineitem-price-multiplier-loader.user.js
    https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/vtiger-lineitem-price-multiplier-loader.user.js

---
+++ vtiger-lineitem-price-multiplier-loader.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         VTiger LineItem Price Multiplier (Loader)
+// @namespace    hw24.lineitem.multiplier.loader
+// @version      1.0.0
+// @description  Loader for the central line item price multiplier userscript
+// @match        https://vtiger.hardwarewartung.com/index.php*
+// @grant        none
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/vtiger-lineitem-price-multiplier.user.js
+// @updateURL    https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/vtiger-lineitem-price-multiplier.user.js
+// @downloadURL  https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/vtiger-lineitem-price-multiplier.user.js
+// ==/UserScript==
