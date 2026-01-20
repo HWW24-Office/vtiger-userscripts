cc9384576adaae2924f36c8687c0c3e6893bf922
Hardwarewartung 24 GmbH <office@hardwarewartung.com>
2026-01-20 15:10:33 +0100

    Update vtiger-maintenance-desc-loader.user.js

--- vtiger-maintenance-desc-loader.user.js
+++ vtiger-maintenance-desc-loader.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         VTiger Maintenance Description Auditor (Loader)
 // @namespace    hw24.vtiger.maintenance.desc
-// @version      0.1.0
+// @version      0.1.1
 // @description  Loader for HW24 VTiger Maintenance Description Auditor (analysis-only)
 // @match        https://vtiger.hardwarewartung.com/index.php*
 // @grant        none
 // @run-at       document-end
 //
-// @require      https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/vtiger-maintenance-desc-auditor.core.v0.1.js
+// @require      https://raw.githubusercontent.com/HWW24-Office/vtiger-userscripts/main/vtiger-maintenance-desc-auditor.core.v0.1.1.js
 //
 // ==/UserScript==
 
